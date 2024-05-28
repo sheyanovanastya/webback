f0be0baefd1d4a825486385ef83f5ac1ea1bb7c4
sheyanovanastya <144317320+sheyanovanastya@users.noreply.github.com>
2024-05-28 15:04:08 +0300

    Delete practychna_8 directory

--- practychna_8/src/link/link.model.ts
+++
@@ -1,9 +0,0 @@
-import { prop, DocumentType } from '@typegoose/typegoose';
-
-export class Link extends DocumentType {
-  @prop({ required: true })
-  url: string;
-
-  @prop({ required: true })
-  userId: number;
-}
--- practychna_8/src/link/link.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { Link } from './link.model';
-
-@Injectable()
-export class LinkService {
-  constructor(@InjectModel('Link') private readonly linkModel: Model<Link>) {}
-}
